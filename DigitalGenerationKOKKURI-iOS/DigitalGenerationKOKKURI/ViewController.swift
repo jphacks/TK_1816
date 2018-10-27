fd52c571f8f6b9656f1d5f7e3a45703be35ec537
murawakimitsuhiro <mitsuhiromurawaki@gmail.com>
2018-10-27 10:25:42 +0900

    Add iOS Project

---
+++ DigitalGenerationKOKKURI-iOS/DigitalGenerationKOKKURI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DigitalGenerationKOKKURI
+//
+//  Created by 村脇光洋 on 2018/10/27.
+//  Copyright © 2018年 村脇光洋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
